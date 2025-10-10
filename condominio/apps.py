3e7c51e8c3f678fcf01d5c315006744aa688ac65
Luis F Blanco <blanco.luis@ficct.uagrm.edu.bo>
2025-10-10 00:49:46 -0400

    suchiful

--- condominio/apps.py
+++ condominio/apps.py
@@ -1,9 +1,9 @@
 from django.apps import AppConfig
 
 
-#class CondominioConfig(AppConfig):
-#    default_auto_field = 'django.db.models.BigAutoField'
-#    name = 'condominio'
-#
-#    def ready(self):
- #       import condominio.signals
+class CondominioConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'condominio'
+
+    def ready(self):
+       import condominio.signals
--- condominio/signals.py
+++ condominio/signals.py
@@ -5,18 +5,18 @@ from django.core.management import call_command
 from django.apps import apps
 
 
-#@receiver(post_migrate)
-#def cargar_datos_iniciales(sender, **kwargs):
-#    """
- #   Carga automáticamente los datos iniciales del fixture después de ejecutar migrate.
-#    Solo se ejecuta cuando se migran los modelos de la app 'condominio'.
-#    """
-#    if sender.name == 'condominio':
-#        Categoria = apps.get_model('condominio', 'Categoria')  
-#        if not Categoria.objects.exists():
-#            try:
-#                print("📦 Cargando datos iniciales para CONDOMINIO...")
-#                call_command('loaddata', 'condominio/fixtures/datos_iniciales.json')
- #               print("✅ Datos iniciales de CONDOMINIO cargados correctamente.")
-#            except Exception as e:
-#                print(f"⚠️ Error al cargar fixtures de condominio: {e}")
+@receiver(post_migrate)
+def cargar_datos_iniciales(sender, **kwargs):
+    """
+    Carga automáticamente los datos iniciales del fixture después de ejecutar migrate.
+    Solo se ejecuta cuando se migran los modelos de la app 'condominio'.
+    """
+    if sender.name == 'condominio':
+        Categoria = apps.get_model('condominio', 'Categoria')  
+        if not Categoria.objects.exists():
+            try:
+                print("📦 Cargando datos iniciales para CONDOMINIO...")
+                call_command('loaddata', 'condominio/fixtures/datos_iniciales.json')
+                print("✅ Datos iniciales de CONDOMINIO cargados correctamente.")
+            except Exception as e:
+               print(f"⚠️ Error al cargar fixtures de condominio: {e}")
